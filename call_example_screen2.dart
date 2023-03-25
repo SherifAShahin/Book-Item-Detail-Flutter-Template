e563e010cd656ba4e2f20ade05f77e205b33bc20
Sherif A Shahin <125300187+SherifAShahin@users.noreply.github.com>
2023-03-25 22:36:03 +0200

    initial commit

---
+++ call_example_screen2.dart
@@ -0,0 +1,63 @@
+// import screen1
+// import book item list template file
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class GitHubScreen2 extends StatelessWidget {
+  BookModel model;
+
+  GitHubScreen2(this.model);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 0.0,
+        title: Text(
+          'BookMark',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: ()
+          {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+          ),
+        ),
+        backgroundColor: HexColor(
+          '#045255',
+        ),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg2.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: booksItemDetailBuilder(
+            model: model,
+            heartIconOnPressed: ()
+            {
+
+            },
+            archiveIconOnPressed: ()
+            {
+
+            },
+            shareIconOnPressed: ()
+            {
+
+            }
+        ),
+      ),
+    );
+  }
+}
